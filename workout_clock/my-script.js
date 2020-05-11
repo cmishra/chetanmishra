739ccf97de8c806e9d0580ebcf03bd997ebe99d4
Chetan Mishra <chetan@instabase.com>
2020-05-10 22:26:49 -0600

    initial commit

---
+++ workout_clock/my-script.js
@@ -0,0 +1,58 @@
+const msg = document.querySelector('.current-message');
+var exercise_img_obj = document.querySelector('.exercise-visual')
+
+var gifs = [
+    './resources/squats.gif',
+    './resources/push ups.gif',
+    './resources/mountain climbers.gif',
+    './resources/rest.gif',
+    './resources/plank.gif',
+    './resources/single leg deadlifts.gif',
+    './resources/single leg hip raises.gif',
+    './resources/rest.gif',
+    './resources/burpees.gif',
+    './resources/split squats.gif',
+    './resources/leg raises.gif',
+    './resources/done.gif'
+]
+
+/*
+TODO 
+ - frame the images of various sizes
+ - test out on phone 
+ - make hip raise gif visible
+ */ 
+
+mins = 0
+seconds = 0
+
+function pad_helper(n){
+    return n.toString().padStart(2, '0')
+}
+
+
+function increment_timer(until_mins) {
+    if (until_mins == mins) {
+        return 
+    }
+    seconds += 1
+    if (seconds >= 60) {
+        mins += 1
+        seconds = 0
+        exercise_img_obj.src = gifs[mins]
+    }
+    msg.innerText = `${pad_helper(mins)}:${pad_helper(seconds)}`
+}
+
+function stop_timer(interval){
+    clearInterval(interval)
+}
+
+function start_timer() {
+        msg.innerText = `${pad_helper(mins)}:${pad_helper(seconds)}`    
+        exercise_img_obj.src = gifs[0]
+        setInterval(increment_timer, 1000, 11)
+}
+
+const button = document.querySelector('.start-timer')
+button.addEventListener('click', start_timer, {once: true})
